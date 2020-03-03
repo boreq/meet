b543ef2d07ef2b28e11e9c4204576692c9a554f9
boreq <boreq@0x46.net>
2020-03-04 00:16:53 +0100

    Add a constructor

--- domain/device.go
+++ domain/device.go
@@ -1,6 +1,19 @@
 package domain
 
+import "github.com/boreq/errors"
+
 type Device struct {
 	uuid     DeviceUUID
 	schedule Schedule
+}
+
+func NewDevice(uuid DeviceUUID, schedule Schedule) (Device, error) {
+	if uuid.IsZero() {
+		return Device{}, errors.New("zero value of uuid")
+	}
+
+	return Device{
+		uuid:     uuid,
+		schedule: schedule,
+	}, nil
 }
